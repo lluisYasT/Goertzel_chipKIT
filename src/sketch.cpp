6a1e9ceb29bb29c79264ff58f1fba7245ca6c7f7
Lluis Satorre <lluisyast@gmail.com>
2013-11-07 11:58:39 +0100

    Un monton de cambios

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -5,9 +5,12 @@
 #include <WProgram.h>
 #include <IOShieldOled.h>
 
-#define N_MUESTRAS	2048
+#define N_MUESTRAS	4096
+#define Fs			27027
+#define PI			3.141592653589793
 
 int16_t muestras[N_MUESTRAS];
+float muestras_norm[N_MUESTRAS];
 
 volatile bool muestras_listas = false;
 volatile uint16_t contador_muestras = 0;
@@ -23,13 +26,55 @@ void setup()
 
 void loop()
 {
+	float frecuencia = 900; //Frecuencia que queremos encontrar en Hz
 	while(!muestras_listas);
 	muestras_listas = false;
 	for (int i = 0; i < N_MUESTRAS; ++i)
 	{
-		Serial.println(muestras[i], DEC);
+		muestras_norm[i] = ((float)muestras[i] - 512.0) / 512.0;
 	}
-	Serial.println();
+
+	float media = 0;
+	for (int i = 0; i < N_MUESTRAS; ++i)
+	{
+		media += abs(muestras_norm[i]);
+	}
+	//Serial.print("Media: ");
+	Serial.print(media / N_MUESTRAS);
+	Serial.print('\t');
+
+	float escalado = N_MUESTRAS / 2;
+
+	float m = frecuencia * N_MUESTRAS / Fs;
+	Serial.print(m);
+	Serial.print('\t');
+
+	float omega = (2.0 * PI * m) / N_MUESTRAS;
+	float coseno = cos(omega);
+	float seno = sin(omega);
+	float coef = 2 * coseno;
+	Serial.print(coef);
+	Serial.print('\t');
+	float w, w_1 = 0, w_2 = 0;
+
+	for (int i = 0; i < N_MUESTRAS; ++i)
+	{
+		w = muestras_norm[i] + coef * w_1 - w_2;
+		w_2 = w_1;
+		w_1 = w;
+	}
+
+	w = coef * w_1 - w_2;
+	w_2 = w_1;
+	w_1 = w;
+
+	//float potencia = w_2 * w_2 + w_1 * w_1 - coef * w_1 * w_2;
+	float real = (w_1 - w_2 * coseno);
+	float imag = (w_2 * seno);
+	float potencia = sqrt(real * real + imag * imag);
+
+	Serial.print("Potencia: ");
+	Serial.println(potencia);
 
 	AD1CON1bits.ASAM = 1; // Comienza auto-muestreo
 }
@@ -63,12 +108,16 @@ void config_analog()
 	// TPB = (1 / 80MHz) * 2 = 25ns
 	// TAD = TPB * (ADCS + 1) * 2 = 25ns * 20  * 2 = 1000ns
 	// TAD = 25ns * 160 * 2 = 8000ns
-	AD1CON3bits.ADCS = 159;
+	AD1CON3bits.ADCS = 19;
 
 	// Tiempo de Auto muestreo
-	// SAMC * TAD = 30 * 1000ns = 30uS 33333KHz
-	// SAMC * TAD = 31 * 8000ns = 248uS 4032.26KHz
-	AD1CON3bits.SAMC = 31;
+	// SAMC * TAD = 30 * 1000ns = 30uS
+	// SAMC * TAD = 31 * 8000ns = 248uS
+	// SAMC * TAD = 25 * 1000ns = 25us
+	AD1CON3bits.SAMC = 25;
+
+	// Tiempo total = Tmuestreo + Tconversion = (TAD * SAMC) + (TAD * 12)
+	// Frecuencia de muestreo = 1 / Tiempo Total
 
 	IPC6bits.AD1IP = 6;		// ADC 1 Prioridad de interrupcion
 	IPC6bits.AD1IS = 3;		// ADC 1 Subprioridad
