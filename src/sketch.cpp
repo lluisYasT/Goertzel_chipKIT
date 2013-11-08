0bdf83dae373ce6e2c870046722fc0cf5c256e0a
Lluis Satorre <lluisyast@gmail.com>
2013-11-08 10:38:07 +0100

    Cambiado el calculo de coeficientes a setup

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -1,12 +1,12 @@
-#if defined(__PIC32MX__)
-    #include <p32xxxx.h>    /* this gives all the CPU/hardware definitions */
-    #include <plib.h>       /* this gives the i/o definitions */
-#endif
+
+#include <p32xxxx.h>    
+#include <plib.h>       
+
 #include <WProgram.h>
 #include <IOShieldOled.h>
 
-#define N_MUESTRAS	4096
-#define Fs			27027
+#define Fs			62500
+#define N_MUESTRAS	256
 #define PI			3.141592653589793
 
 int16_t muestras[N_MUESTRAS];
@@ -14,6 +14,9 @@ float muestras_norm[N_MUESTRAS];
 
 volatile bool muestras_listas = false;
 volatile uint16_t contador_muestras = 0;
+float m;
+float omega, coseno, seno, coef, frecuencia;
+char frec[12];
 
 void config_analog(void);
 
@@ -22,11 +25,20 @@ void setup()
 	IOShieldOled.begin();
 	Serial.begin(115200);
 	config_analog();
+
+	omega = (2.0 * PI * m) / N_MUESTRAS;
+	coseno = cos(omega);
+	seno = sin(omega);
+	coef = 2 * coseno;
+
+	m = 2;
+
+	frecuencia = m * (float)Fs / N_MUESTRAS;
+	sprintf(frec, "%.1f Hz",frecuencia);
 }
 
 void loop()
 {
-	float frecuencia = 900; //Frecuencia que queremos encontrar en Hz
 	while(!muestras_listas);
 	muestras_listas = false;
 	for (int i = 0; i < N_MUESTRAS; ++i)
@@ -34,27 +46,6 @@ void loop()
 		muestras_norm[i] = ((float)muestras[i] - 512.0) / 512.0;
 	}
 
-	float media = 0;
-	for (int i = 0; i < N_MUESTRAS; ++i)
-	{
-		media += abs(muestras_norm[i]);
-	}
-	//Serial.print("Media: ");
-	Serial.print(media / N_MUESTRAS);
-	Serial.print('\t');
-
-	float escalado = N_MUESTRAS / 2;
-
-	float m = frecuencia * N_MUESTRAS / Fs;
-	Serial.print(m);
-	Serial.print('\t');
-
-	float omega = (2.0 * PI * m) / N_MUESTRAS;
-	float coseno = cos(omega);
-	float seno = sin(omega);
-	float coef = 2 * coseno;
-	Serial.print(coef);
-	Serial.print('\t');
 	float w, w_1 = 0, w_2 = 0;
 
 	for (int i = 0; i < N_MUESTRAS; ++i)
@@ -68,13 +59,20 @@ void loop()
 	w_2 = w_1;
 	w_1 = w;
 
-	//float potencia = w_2 * w_2 + w_1 * w_1 - coef * w_1 * w_2;
 	float real = (w_1 - w_2 * coseno);
 	float imag = (w_2 * seno);
 	float potencia = sqrt(real * real + imag * imag);
 
-	Serial.print("Potencia: ");
-	Serial.println(potencia);
+	IOShieldOled.clear();
+	IOShieldOled.setCursor(0,0);
+	
+	IOShieldOled.putString(frec);
+	char pot[8];
+	IOShieldOled.setCursor(0,1);
+	sprintf(pot, "%.2f", potencia);
+	IOShieldOled.putString(pot);
+
+	delay(50);
 
 	AD1CON1bits.ASAM = 1; // Comienza auto-muestreo
 }
@@ -101,11 +99,12 @@ void config_analog()
 	//Configuracion del ADC
 	AD1PCFG = ~1;	// PORTB digital, RB0 analogico
 	AD1CHSbits.CH0SA = 1; // Canal 1. AN1
+	AD1CON2bits.SMPI = 0; // Cantidad de conversiones anates de generar la interrupcion
 	AD1CON1bits.SSRC  = 7; // El contador interno termina el muestreo y comienza la conversion.
-	AD1CON1bits.ASAM = 1; // El muestreo comienza cuando se active el bit SAMP
+	AD1CON1bits.ASAM = 1; // El muestreo comienza cuando inmediatamente despues de la conversion.
 
 	// Seleccion el reloj del ADC
-	// TPB = (1 / 80MHz) * 2 = 25ns
+	// TPB = (1 / 80MHz) * 2 = 25ns <- Periodo del reloj del bus de perifericos
 	// TAD = TPB * (ADCS + 1) * 2 = 25ns * 20  * 2 = 1000ns
 	// TAD = 25ns * 160 * 2 = 8000ns
 	AD1CON3bits.ADCS = 19;
@@ -114,7 +113,7 @@ void config_analog()
 	// SAMC * TAD = 30 * 1000ns = 30uS
 	// SAMC * TAD = 31 * 8000ns = 248uS
 	// SAMC * TAD = 25 * 1000ns = 25us
-	AD1CON3bits.SAMC = 25;
+	AD1CON3bits.SAMC = 4;
 
 	// Tiempo total = Tmuestreo + Tconversion = (TAD * SAMC) + (TAD * 12)
 	// Frecuencia de muestreo = 1 / Tiempo Total
