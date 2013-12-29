022c9b625ee4eaef57420c7ca3988e76d85254db
Lluis <lluisyast@gmail.com>
2013-12-29 23:18:04 +0100

    Cambiada frecuencia de muestreo a 96620Hz (falta calcular la atenuación correcta)

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -1,15 +1,18 @@
 #include <plib.h>       
 #include <WProgram.h>
 
+//Fs = 47.6 kHz
+
 #define GAIN_BITS		15
 #define GAIN				(1<<GAIN_BITS)
-#define N_MUESTRAS	800
-#define COSENO			19261	// 0.5878 * 2^15 1.2kHz
-#define SENO				26510 // 0.809 * 2^15
+#define N_MUESTRAS	4810  // Fs / 20 > Frec fundamental = 20Hz
+#define COSENO			32668	// 0.996957 * 2^15 1.2kHz
+#define SENO				2554	// 0.079567 * 2^15
 #define MITAD				16384 // 0.5 * 2^15
 #define ATENUADOR		57		// Para no saturar
 #define UMBRAL			2949	// 0.3^2 * 2^15
 
+volatile int muestras[N_MUESTRAS];
 volatile bool muestras_listas = false;
 volatile uint16_t contador_muestras = 0;
 volatile int max = 0;
@@ -27,25 +30,52 @@ void setup()
 	// Seleccion el reloj del ADC
 	// TPB = (1 / 80MHz) = 12.5ns <- Periodo del reloj del bus de perifericos
 	// TAD = TPB * (ADCS + 1) * 2
-	AD1CON3bits.ADCS = 255;
+	AD1CON3bits.ADCS = 22;
 
 	// Tiempo de Auto muestreo
 	// Tmuestreo = SAMC * TAD
-	AD1CON3bits.SAMC = 26;
+	AD1CON3bits.SAMC = 23;
 
-	// Frecuencia de muestreo = 8kHz
+	// Frecuencia de muestreo = 96.62 kHz (Medido con UT61E)
 
 	IPC6bits.AD1IP = 6;		// ADC 1 Prioridad de interrupcion
 	IPC6bits.AD1IS = 3;		// ADC 1 Subprioridad
 	IFS1CLR = 2;
 	IEC1bits.AD1IE = 1;		// Habilitamos la interrupcion del ADC
 	AD1CON1bits.ON = 1;		// Habilitamos el ADC
+
+	TRISAbits.TRISA2 = 0;
+//	Serial.begin(115200);
 }
 
 void loop()
 {
+	uint16_t i;
 	while(!muestras_listas);
 	muestras_listas = false;
+
+	for (i = 0; i < N_MUESTRAS; i++) {
+		//Serial.print(muestras[i]);
+		//Serial.print(',');
+		muestras[i] = muestras[i] - 512;
+		muestras[i] = muestras[i] << (GAIN_BITS - 9);
+		muestras[i] *= ATENUADOR;
+		muestras[i] >>= GAIN_BITS;
+		
+		if(muestras[i] > max) {
+			max = muestras[i];
+		}
+
+		// Parte recursiva de Goertzel
+		q0 = (COSENO * q1);
+		q0 -= (MITAD * q2);
+		q0 += (COSENO * q1);
+		q0 -= (MITAD * q2);
+		q0 >>= GAIN_BITS;
+		q0 += muestras[i];
+		q2 = q1;
+		q1 = q0;
+	}
 
 	// Parte no recursiva de Goertzel
 	q0 = (COSENO * q1);
@@ -93,25 +123,9 @@ extern "C"
 	{
 		IFS1CLR = 2;
 
-		int muestra = ADC1BUF0;
-		muestra = muestra - 512;
-		muestra = muestra << (GAIN_BITS - 9);
-		muestra *= ATENUADOR;
-		muestra >>= GAIN_BITS;
-		
-		if(muestra > max) {
-			max = muestra;
-		}
-
-		// Parte recursiva de Goertzel
-		q0 = (COSENO * q1);
-		q0 -= (MITAD * q2);
-		q0 += (COSENO * q1);
-		q0 -= (MITAD * q2);
-		q0 >>= GAIN_BITS;
-		q0 += muestra;
-		q2 = q1;
-		q1 = q0;
+		//int muestra = ADC1BUF0;
+		muestras[contador_muestras] = ADC1BUF0;
+		mPORTAToggleBits(BIT_2);
 
 		contador_muestras++;
 
